9337187c46d4e3073333acd55dd228f9f7d45a99
koudaihirata <2230051@ecc.ac.jp>
2023-11-10 14:55:47 +0900

    firebaseにtextで打った文字を保存でけた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
+"use client";
 import Image from "next/image";
+import { doc, getDoc , setDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleClick = async (e:any) => {
+    e.preventDefault();
+    console.log("click");
+
+    const docRef = doc(db, "koudai", "new");
+    const docSnap = await getDoc(docRef);
+
+    await setDoc(doc(db, "koudai", "new"), {
+      name: inputValue, // テキスト入力フィールドの値をFirebaseに保存
+      state: "CA",
+      country: "USA"
+    });
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="w-full h-screen">
+        <h1>Hello World</h1>
+          <form action="">
+            <input type="text" name="name" className="text"
+              onChange={(e) => setInputValue(e.target.value)}/>
+            <br />
+            <button className="btn" type="submit"
+              onClick={handleClick} // 送信ボタンがクリックされたときにhandleClick関数を呼び出す
+            >送信</button>
+          </form>
+      </div>
     </>
   );
 }
---
+++ src/firebase/firebase.tsx
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrk01UlBFwJ0cAVprODay6BLY2p0NSE0A",
+    authDomain: "yurumetabi-2fe91.firebaseapp.com",
+    projectId: "yurumetabi-2fe91",
+    storageBucket: "yurumetabi-2fe91.appspot.com",
+    messagingSenderId: "1069942845228",
+    appId: "1:1069942845228:web:72b4adb99f116607ee0689",
+    measurementId: "G-XRFK3KXT3M"
+}
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
