c0eff15c241051d9cd848a5574d4be6f432c6de8
koudaihirata <2230051@ecc.ac.jp>
2023-11-17 09:51:52 +0900

    fest:ファイル追加しました

---
+++ src/feature/categoryBtn/index.tsx
@@ -0,0 +1,13 @@
+
+
+export default function CategoryBtn() {
+    return <>
+        <p>
+            <ul>
+                <li>
+                    <p>飲食店</p>
+                </li>
+            </ul>
+        </p>
+    </>;
+}
