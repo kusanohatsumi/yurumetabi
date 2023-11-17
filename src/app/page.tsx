1476ff44f757aa1c16149f0e79e4a68748cd6b0c
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-11-17 09:23:52 +0900

    add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+
+export function click(e: any) {
+  console.log(e.target);
+}
+
+export default async function Home() {
+  const docRef = doc(db, "sample", "newItem");
+  const docSnap = await getDoc(docRef);
+  // console.log(docSnap.data());
+
+  // if (docSnap.exists()) {
+  //   console.log("Document data:", docSnap.data());
+  // } else {
+  //   console.log("No such document!");
+  // }
+
+  await setDoc(doc(db, "sample", "newItem"), {
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA",
+  });
+
+  let AA = "aaa";
+  return <main></main>;
 }
