8dbc7c7bf9a21e56c0b2f995505f1d28f877c54f
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-11-18 14:18:57 +0900

    変更の適応

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
