027de0a5809d23ae7b1db83d4d9da8a750ab996d
koudaihirata <2230051@ecc.ac.jp>
2023-11-24 11:23:56 +0900

    ページ修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
- import CameraScreen from "@/feature/CameraScreen";
+import CameraScreen from "@/feature/CameraScreen";
 import Header_main from "@/feature/header";
 import Menu from "@/feature/menu";
 import PR from "@/feature/PR";
