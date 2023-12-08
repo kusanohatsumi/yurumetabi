836793328aee52ce63187003ebc83734ed78f8ba
koudaihirata <2230051@ecc.ac.jp>
2023-12-08 12:21:14 +0900

    シェア登録ページのコンポーネント完成

--- src/feature/CategoryBtn/index.tsx
+++ src/feature/CategoryBtn/index.tsx
@@ -1,28 +1,43 @@
 "use client";
 
-import React, { Children, useState } from 'react';
+import React, { useState } from 'react';
 
 export default function CategoryBtn({
     children,
-}:{children: React.ReactNode}){
+    onClick,
+    selected, // selectedプロパティを追加します。
+}:{children: React.ReactNode, onClick?:() => void, selected?: boolean}){ // selectedプロパティの型を追加します。
     const [color, setColor] = useState('#B99162');
+    const [shadow, setShadow] = useState('0px 1px 1px 0px #777777')
 
-    const style = {
+    // 選択状態に基づいてスタイルを更新します。
+    React.useEffect(() => {
+        if (selected) {
+            setColor('#A98C65');
+            setShadow('none');
+        } else {
+            setColor('#B99162');
+            setShadow('0px 1px 1px 0px #777777');
+        }
+    }, [selected]);
+
+    const styles = {
         color: '#fefefe',
         backgroundColor: color,
         border: '.5px solid #fefefe',
         borderRadius: '16px',
         paddingLeft: '8px',
-        paddingRight: '8px'
+        paddingRight: '8px',
+        boxShadow: shadow
     }
 
     const handleClick = () => {
-        setColor('#A98C65');
+        onClick && onClick();
     }
 
     return(
         <>
-            <button style={style} onClick={handleClick}>{children}</button>
+            <button style={styles} onClick={handleClick}>{children}</button>
         </>
     )
 }
--- src/feature/PhotoTaken/index.tsx
+++ src/feature/PhotoTaken/index.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { useState } from "react"
 import "@/feature/PhotoTaken/styli.css"
 import CategoryBtn from "../CategoryBtn"
 import Link from "next/link"
 
 export default function PhotoTaken() {
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [emotion, setEmotion] =useState("");
+
+    const categories = ['飲食店', '雑貨屋', '服屋', '街並み', '絶景', '有名な観光地', 'その他'];
+    const emotions = ['面白い','可愛い','かっこいい','おしゃれ','落ち着く','レトロ','その他']
+
     return(
         <>
             <div className="box">
@@ -12,8 +21,27 @@ export default function PhotoTaken() {
                 </section>
                 <section className="category">
                     <p>この場所のカテゴリーを選択してください</p>
-                    <div className="place"><CategoryBtn>tttt</CategoryBtn></div>
-                    <div></div>
+                    <div className="place">
+                        {categories.map((category, index) => (
+                        <CategoryBtn
+                        key={index}
+                        onClick={() => setSelectedCategory(category)}
+                        selected={selectedCategory === category} // selectedプロパティを追加します。
+                        >
+                        {category}
+                        </CategoryBtn>
+                        ))}                    
+                    </div>
+                    <div className="emotions">
+                        {emotions.map((category, index) => (
+                            <CategoryBtn
+                            key={index} onClick={() => setEmotion(category)}
+                            selected={emotion === category}
+                        >
+                            {category}
+                        </CategoryBtn>
+                        ))}
+                    </div>
                 </section>
                 <section className="confirmationBtn">
                     <p><Link href="#">確認</Link></p>
