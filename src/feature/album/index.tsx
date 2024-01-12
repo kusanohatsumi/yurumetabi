deae0e9abed5b3d4975ab4e5a0b25fc5df63369c
koudaihirata <2230051@ecc.ac.jp>
2024-01-12 09:46:19 +0900

    画像保存を試行錯誤中

--- src/feature/album/index.tsx
+++ src/feature/album/index.tsx
@@ -2,7 +2,7 @@
 import "@/feature/album/style.css"
 import Image from "next/image"
 import { useEffect, useState } from "react"
-import { DocumentData, collection, doc, getDocs } from "firebase/firestore"
+import { DocumentData, collection, setDoc, doc, getDocs, getDoc, deleteDoc } from "firebase/firestore"
 import { db } from "@/firebase/firebase"
 
 
@@ -12,21 +12,46 @@ export default function Album() {
     const [ shares, setShares ] = useState<DocumentData[]>([]);
     // useEffectフックを使用して、コンポーネントがマウントされた後に実行される処理を定義します
     useEffect(() => {
-        // FireStoreのデータベースから'share'コレクションの参照を取得します
-        const shareRef = collection(db, 'share');
-        // 'share'コレクションのすべてのドキュメントを取得し、それぞれのドキュメントのデータをコンソールに出力します
-        getDocs(shareRef).then((QuerySnapshot) => {
-            QuerySnapshot.docs.forEach((doc) => console.log(doc.data()));
-        });
-        // 'share'コレクションのすべてのドキュメントを再度取得し、それぞれのドキュメントのデータとIDを含む新しいオブジェクトを作成します
-        // これらの新しいオブジェクトを含む配列を作成し、それを'shares' stateに設定します
-        getDocs(shareRef).then((querySnapshot) => {
-            const date = querySnapshot.docs.map((doc) => ({ ...doc.data(), id:doc.id}));
-
-            setShares(date);
-        });
-    // useEffectフックの第二引数に空の配列を渡すことで、このフック内の処理はコンポーネントがマウントされた時に一度だけ実行されます
-    },[]);
+        // 'counters'コレクションの'shareCount'ドキュメントの参照を取得します
+        const counterRef = doc(db, 'counters', 'shareCount');
+    // 'shareCount'ドキュメントのデータを取得します
+    getDoc(counterRef).then((docSnapshot) => {
+        if (docSnapshot.exists()) {
+            // 'shareCount'ドキュメントのデータを取得します
+            const data = docSnapshot.data();
+            console.log(data); // ドキュメントのデータをログに出力します
+            const count = data.count;
+            console.log(typeof count); 
+            // 'shareCount'が数値であることを確認します
+            if (typeof count === 'number') {
+                const decrementedShareCount = count - 1;
+                // 'share'コレクションの参照を取得します
+                const shareRef = collection(db, 'share');
+                // 'share'コレクションから特定のドキュメントを取得します
+                const specificShareRef = doc(shareRef, `share${decrementedShareCount}`);
+                // 特定のドキュメントのデータを取得します
+                getDoc(specificShareRef).then((docSnapshot) => {
+                    if (docSnapshot.exists()) {
+                        // ドキュメントのデータを取得し、それを'shares' stateに設定します
+                        const data = docSnapshot.data();
+                        setShares([data]);
+                    } else {
+                        // ドキュメントが存在しない場合、エラーメッセージをログに出力します
+                        console.log(`share${decrementedShareCount} ドキュメントは存在しません`);
+                    }
+                });
+            } else {
+                // 'shareCount'が数値でない場合、エラーメッセージをログに出力します
+                console.log("'shareCount'は数値ではありません");
+                console.log(typeof shares); // 'shares'の型をログに出力します
+                console.log(typeof data.shareCount); // 'shareCount'プロパティの型をログに出力します                
+            }
+        } else {
+            // 'shareCount'ドキュメントが存在しない場合、エラーメッセージをログに出力します
+            console.log("'shareCount' ドキュメントは存在しません");
+        }
+    });
+    }, []);
 
 
     return(
