9cdc781dfd60008aca18be717232e4606129bc08
koudaihirata <2230051@ecc.ac.jp>
2024-01-26 12:16:59 +0900

    ?

--- src/feature/mypage/titleStyle/index.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function TitleStyle() {
-  return (
-    <>
-      <Image alt="" src="/image/account_icon.svg" width={64} height={64} />
-    </>
-  );
-}
